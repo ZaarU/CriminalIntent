e87eb2fa45fc521b5b5827b754b9c2e2465a6d95
Владимир Геращенко <gerash@ua.fm>
2016-11-24 10:15:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/zaaru/criminalintent/CrimeActivity.java
@@ -0,0 +1,12 @@
+package com.example.zaaru.criminalintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CrimeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
