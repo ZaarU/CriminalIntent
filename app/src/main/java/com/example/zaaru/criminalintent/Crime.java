6c93e8b3f182a6142dcf922638280704abf37da2
Владимир Геращенко <gerash@ua.fm>
2016-12-19 09:32:44 +0200

    Add CrimeFragment Add 2 layout

--- app/src/main/java/com/example/zaaru/criminalintent/Crime.java
+++ app/src/main/java/com/example/zaaru/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.zaaru.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,13 +10,33 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean ismSolved() {
+        return mSolved;
+    }
+
+    public void setmSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
 
     public UUID getmId() {
         return mId;
     }
+
     public String getmTitle() {
         return mTitle;
     }
+
     public void setmTitle(String mTitle) {
         this.mTitle = mTitle;
     }
@@ -23,5 +44,6 @@ public class Crime {
     public Crime(){
         // Генерация уникального идентификатора
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 }
